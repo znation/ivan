fcfd4797bfaad8bd7f9fd2dc25de28d42d9e1d25
Zach Nation <znation@gmail.com>
2026-04-30 03:54:43 -0700

    Make dbgmsg.h macros print debug messages instead of being no-ops

--- FeLib/Include/dbgmsg.h
+++ FeLib/Include/dbgmsg.h
@@ -1,11 +1,12 @@
 /**
- * Stub dbgmsg.h - provides no-op implementations for debugging macros
+ * dbgmsg.h - provides active implementations for debugging macros
  * and the dbgmsg namespace when DBGMSG is defined.
  */
 
 #ifndef INCLUDE_DBGMSG_H_
 #define INCLUDE_DBGMSG_H_
 
+#include <iostream>
 #include <sstream>
 #include <string>
 
@@ -20,38 +21,67 @@ inline std::string getCurrentStackTrace(std::string, bool, bool) { return ""; }
 
 inline void SigHndlr(int) {}
 
+template <typename... Args>
+void PrintDebug(Args&&... args) {
+    std::ostringstream oss;
+    (oss << ... << args);
+    std::cerr << "[DBG] " << oss.str() << std::endl;
+}
+
 } /* namespace dbgmsg */
 
-/* ---- Debug macro stubs (used when DBGMSG is defined) ---- */
+/* ---- Debug macros (used when DBGMSG is defined) ---- */
 
-#define DBGOE(s)
-#define DBGSS(s)
-#define DBG1(a)
-#define DBG2(a, b)
-#define DBG3(a, b, c)
-#define DBG4(a, b, c, d)
-#define DBG5(a, b, c, d, e)
-#define DBG6(a, b, c, d, e, f)
-#define DBG7(a, b, c, d, e, f, g)
-#define DBG8(a, b, c, d, e, f, g, h)
-#define DBG9(a, b, c, d, e, f, g, h, i)
+#define DBGOE(s) \
+    do { std::cerr << "[DBG] ERROR: " << (s) << std::endl; } while(0)
+#define DBGSS(s) \
+    do { std::cerr << "[DBG] " << (s) << std::endl; } while(0)
+#define DBG1(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG2(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG3(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG4(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG5(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG6(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG7(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG8(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
+#define DBG9(a, ...) \
+    do { dbgmsg::PrintDebug(a, ##__VA_ARGS__); } while(0)
 
-#define DBGLN
+#define DBGLN std::cerr << std::endl
 #define DBGTOSTR_(str) #str
 #define DBGTOSTR(str) DBGTOSTR_(str)
-#define DBGB(B)
-#define DBGSB(B)
-#define DBGI(I)
-#define DBGSI(I)
-#define DBGIF(F)
-#define DBGSF(F)
-#define DBGC(C)
-#define DBGSC(C)
-#define DBGS(SS)
-#define DBGSTK
-#define DBGBREAKPOINT
-#define DBGEXEC(cmds)
-#define DBGSETV(id, val)
+#define DBGB(B) \
+    do { std::cerr << "[DBG] " << (B) << std::endl; } while(0)
+#define DBGSB(B) \
+    do { std::cerr << "[DBG] " << (B) << std::endl; } while(0)
+#define DBGI(I) \
+    do { std::cerr << "[DBG] " << (I) << std::endl; } while(0)
+#define DBGSI(I) \
+    do { std::cerr << "[DBG] " << (I) << std::endl; } while(0)
+#define DBGIF(F) \
+    do { std::cerr << "[DBG] " << (F) << std::endl; } while(0)
+#define DBGSF(F) \
+    do { std::cerr << "[DBG] " << (F) << std::endl; } while(0)
+#define DBGC(C) \
+    do { std::cerr << "[DBG] " << (C) << std::endl; } while(0)
+#define DBGSC(C) \
+    do { std::cerr << "[DBG] " << (C) << std::endl; } while(0)
+#define DBGS(SS) \
+    do { std::cerr << "[DBG] " << (SS) << std::endl; } while(0)
+#define DBGSTK \
+    do { std::cerr << "[DBG] Stack: " << dbgmsg::getCurrentStackTrace(true, 5) << std::endl; } while(0)
+#define DBGBREAKPOINT \
+    do { __builtin_trap(); } while(0)
+#define DBGEXEC(cmds) cmds
+#define DBGSETV(id, val) ((void)0)
 #define DBGGETV(id, defval) (defval)
 #define DBGGETVD(id, defval) (defval)
 
